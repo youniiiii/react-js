e621851a1604c71457975642e2730ca477997df2
jonatan mansilla <yoniii-11@live.com.ar>
2022-09-01 04:05:40 -0300

    soy el nav

--- src/App.js
+++ src/App.js
@@ -3,21 +3,43 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div classNameName="App">
+    <nav className="navbar navbar-dark bg-dark navbar-expand-lg ">
+    <div className="container-fluid">
+      <a className="navbar-brand" href="#"> <img src={logo} className="App-logo" alt="logo" width="30px" height="30px"/></a>
+      <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+        <span className="navbar-toggler-icon"></span>
+      </button>
+      <div className="collapse navbar-collapse" id="navbarSupportedContent">
+        <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+          <li className="nav-item">
+            <a className="nav-link active" aria-current="page" href="#">mi tienda</a>
+          </li>
+          <li className="nav-item">
+            <a className="nav-link" href="#">paginas</a>
+          </li>
+          <li className="nav-item dropdown">
+            <a className="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+             catalogos
+            </a>
+            <ul className="dropdown-menu">
+              <li><a className="dropdown-item" href="#">ropa</a></li>
+              <li><a className="dropdown-item" href="#">electrodomesticos</a></li>
+      
+              <li><a className="dropdown-item" href="#">alimentos</a></li>
+            </ul>
+          </li>
+          <li className="nav-item">
+            <a className="nav-link ">carrito</a>
+          </li>
+        </ul>
+        <form className="d-flex" role="search">
+          <input className="form-control me-2" type="search" placeholder="buscar" aria-label="Search"></input>
+          <button className="btn btn-outline-success" type="submit">buscar</button>
+        </form>
+      </div>
+    </div>
+  </nav>
     </div>
   );
 }
